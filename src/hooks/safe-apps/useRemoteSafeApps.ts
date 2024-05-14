f1ad1dc2672fa0d92c8796af65d4f27ee30f1e6a
Nicholas Rodrigues Lordello <nick@safe.global>
2024-05-14 10:57:09 +0200

    Hardcode Transaction BUilder App

--- src/hooks/safe-apps/useRemoteSafeApps.ts
+++ src/hooks/safe-apps/useRemoteSafeApps.ts
@@ -9,7 +9,65 @@ import type { SafeAppsTag } from '@/config/constants'
 
 // To avoid multiple simultaneous requests (e.g. the Dashboard and the SAFE header widget),
 // cache the request promise for 100ms
-let cache: Record<string, Promise<SafeAppsResponse> | undefined> = {}
+let cache: Record<string, Promise<SafeAppsResponse> | undefined> = {
+  "41144114": Promise.resolve([{
+    "id":29,
+    "url":"https://apps-portal.safe.global/tx-builder",
+    "name":"Transaction Builder",
+    "iconUrl":"https://safe-transaction-assets.safe.global/safe_apps/29/icon.png",
+    "description":"Compose custom contract interactions and batch them into a single transaction",
+    "chainIds":[
+      "1",
+      "4",
+      "10",
+      "56",
+      "100",
+      "137",
+      "246",
+      "324",
+      "1101",
+      "8453",
+      "84532",
+      "42161",
+      "42220",
+      "43114",
+      "73799",
+      "84531",
+      "1313161554",
+      "11155111",
+      "41144114"
+    ],
+    "provider":null,
+    "accessControl":{
+      "type":"NO_RESTRICTIONS",
+      "value":null
+    },
+    "tags":[
+      "dashboard-widgets",
+      "Infrastructure",
+      "transaction-builder"
+    ],
+    "features":[
+      "BATCHED_TRANSACTIONS"
+    ],
+    "developerWebsite":"https://safe.global",
+    "socialProfiles":[
+      {
+        "platform":"DISCORD",
+        "url":"https://chat.safe.global"
+      },
+      {
+        "platform":"GITHUB",
+        "url":"https://github.com/safe-global"
+      },
+      {
+        "platform":"TWITTER",
+        "url":"https://twitter.com/safe"
+      }
+    ]
+  }]),
+}
+
 const cachedGetSafeApps = (chainId: string): ReturnType<typeof getSafeApps> | undefined => {
   if (!cache[chainId]) {
     cache[chainId] = getSafeApps(chainId, { client_url: window.location.origin })
